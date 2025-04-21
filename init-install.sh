309e7007576f3d0bb57fd9d1fef82856d664ce27
0ne-zero <pouria.khakpour9909@gmail.com>
2025-04-21 19:10:11 +0330

    Add init-install.sh

---
+++ init-install.sh
@@ -0,0 +1,19 @@
+git clone https://github.com/blechschmidt/massdns.git; cd massdns; make; sudo make install
+
+pip3 install dnsgen
+
+curl https://sh.rustup.rs -sSf | sh
+
+cargo install x8
+
+go install github.com/projectdiscovery/alterx/cmd/alterx@latest
+go install github.com/tomnomnom/anew@latest
+go install github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest
+go install github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+go install github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install github.com/tomnomnom/unfurl@latest
+go install github.com/tomnomnom/gf@latest
+go install github.com/tomnomnom/meg@latest
+go install github.com/ffuf/ffuf@latest
+go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
